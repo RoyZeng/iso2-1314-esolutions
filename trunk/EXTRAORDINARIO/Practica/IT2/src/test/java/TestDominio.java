a223767e6674f8bc2e4e8462c4a4109801fb55d4
base.dks@gmail.com <base.dks@gmail.com@28c6c0bb-d215-0398-b413-eadc6051195e>
2014-06-10 12:20:07 +0000

    update general Final

---
+++ trunk/EXTRAORDINARIO/Practica/IT2/src/test/java/TestDominio.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import interfaz.VentanaLogin;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dominio.AgenteProducto;
+import dominio.Producto;
+
+
+public class TestDominio {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testloginInterfaz() {
+		try {
+			Assert.assertEquals(VentanaLogin.validarUsuario("05713540","9999"),true);
+Assert.assertEquals(VentanaLogin.validarUsuario("05713540","8999"),false);
+		} catch (IOException e) {
+			fail(e.toString());
+		}
+	}
+	@Test
+	public void testAñadeProducto() throws IOException {
+		AgenteProducto agente =new AgenteProducto();
+		agente.insertProducto(new Producto(0,"nombre",1,"tipo","fecha"));
+		assertTrue(true);
+	}
+
+}
